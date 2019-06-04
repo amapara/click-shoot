aa967d9e3fa63bae3fe80dab8ad5087c9ecdf88d
Alpesh Mapara <alpesh.m@btinternet.com>
2019-06-04 10:02:01 -0400

    corrected code

--- db/migrate/20190604134211_create_bookings.rb
+++ db/migrate/20190604134211_create_bookings.rb
@@ -1,12 +1,12 @@
 class CreateBookings < ActiveRecord::Migration[5.2]
   def change
     create_table :bookings do |t|
-      t.references, :sale_item_id
-      t.date, :start_date
-      t.date, :end_date
-      t.references, :user_id
-      t.float, :total_price
-      t.string, :pick_up_location
+      t.references :sale_item, foreign_key: true
+      t.date :start_date
+      t.date :end_date
+      t.references :user, foreign_key: true
+      t.float :total_price
+      t.string :pick_up_location
       t.string :drop_off_location
 
       t.timestamps
