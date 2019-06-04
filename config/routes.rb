b56d94ce4e5e8b83b654ed3479f8223a60ea3844
Alastair Hewitt <alastairjohnhewitt@gmail.com>
2019-06-04 15:02:00 -0300

    sales routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,13 @@ Rails.application.routes.draw do
     resources :bookings
    end
    get '/dashboard', to: 'dashboards#view'
+
+   #intention: address of T&C page
+   get 'pages/sell_w_us', to: 'pages#sell_w_us'
+
+   #intention: create new listing with user upload
+   post '/listings/create', to: 'listings#create'
+
+   #intention: add newly created listing camera index w/ confirmation
+   patch '/listing/:id', to: 'listings#index'
 end
