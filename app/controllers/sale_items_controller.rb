daf57d3cd3bd68269468b32eb7035f63898938be
Alastair Hewitt <alastairjohnhewitt@gmail.com>
2019-06-05 13:21:25 -0300

    basic dashboard

--- app/controllers/sale_items_controller.rb
+++ app/controllers/sale_items_controller.rb
@@ -1,12 +1,12 @@
-class SaleItemsController < ApplicationControlle
-
+class SaleItemsController < ApplicationController
   def new
   end
+
   def index
     @sale_items = SaleItem.all
   end
 
   def show
-    @sale_item= SaleItem.find(params[:id])
+    @sale_item = SaleItem.find(params[:id])
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -36,7 +36,6 @@ ActiveRecord::Schema.define(version: 2019_06_04_134211) do
     t.bigint "user_id"
     t.date "start_date"
     t.date "end_date"
-    t.string "city"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_sale_items_on_user_id"
@@ -45,8 +44,6 @@ ActiveRecord::Schema.define(version: 2019_06_04_134211) do
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
-    t.string "first_name"
-    t.string "last_name"
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
