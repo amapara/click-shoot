3da9083460897e87c930ff212edb5c76a077fca4
Alpesh Mapara <alpesh.m@btinternet.com>
2019-06-06 13:41:50 -0400

    tinkered with new sales items journey a bit

--- app/models/sale_item.rb
+++ app/models/sale_item.rb
@@ -4,4 +4,9 @@ class SaleItem < ApplicationRecord
   mount_uploader :photo, PhotoUploader
 
   validates :model, presence: true
+  validates :set_up_rate, presence: true
+  validates :daily_rate, presence: true
+  validates :start_date, presence: true
+  validates :end_date, presence: true
+  validates :city, presence: true
 end
